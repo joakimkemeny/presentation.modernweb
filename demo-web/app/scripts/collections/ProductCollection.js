4ac16768e926f98308523a3162db788cb27b5d73
Joakim Kemeny <joakim@kemeny.se>
2013-01-30 17:12:43 +0100

    Use WebSockets in the interface.

--- demo-web/app/scripts/collections/ProductCollection.js
+++ demo-web/app/scripts/collections/ProductCollection.js
@@ -1,12 +1,21 @@
 define([
     "backbone",
+    "framework/WebSocket",
     "models/ProductModel"
-], function (Backbone, ProductModel) {
+], function (Backbone, webSocket, ProductModel) {
     "use strict";
 
     var ProductCollection = Backbone.Collection.extend({
         url : "http://local.communityhack.org/api/product",
-        model : ProductModel
+        model : ProductModel,
+
+        initialize : function () {
+            webSocket.on("stockStatusUpdated", this.updateStockStatus, this);
+        },
+
+        updateStockStatus : function (data) {
+            this.get(data.id).set("stockStatus", data.stockStatus);
+        }
     });
 
     return ProductCollection;
